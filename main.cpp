1561f77c3b02aa81490198a9d6195e984ebf15fc
Budazhap <zandakovbuda@gmail.com>
2025-12-15 18:24:45 +0700

    commit 5

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <utility>
 
 #include "MyAllocator.hpp"
 #include "MyContainer.hpp"
@@ -12,12 +13,17 @@ long long factorial(int n) {
 }
 
 int main() {
-    std::cout << "std::map with default allocator\n";
+    std::cout << "std::map со стандартным аллокатором\n";
     std::map<int, int> m1;
     for (int i = 0; i < 10; ++i)
         m1[i] = factorial(i);
+    
+    std::cout << "Вывод содержимого m1:\n";
+    for (auto& [k, v] : m1)
+        std::cout << " " << k << " " << v << "\n";
+    
 
-    std::cout << "std::map with custom allocator\n";
+    std::cout << "std::map с пользовательским аллокатором\n";
     using Pair = std::pair<const int, int>;
     using Alloc = MyAllocator<Pair, 10>;
     std::map<int, int, std::less<int>, Alloc> m2;
@@ -25,19 +31,28 @@ int main() {
     for (int i = 0; i < 10; ++i)
         m2[i] = factorial(i);
 
+    std::cout << "Вывод содержимого m2:\n";
     for (auto& [k, v] : m2)
-        std::cout << k << " " << v << "\n";
+        std::cout << " " << k << " " << v << "\n";
 
-    std::cout << "MyContainer with default allocator\n";
+    std::cout << "MyContainer со стандартным аллокатором\n";
     MyContainer<int> c1;
     for (int i = 0; i < 10; ++i)
         c1.push_back(i);
 
-    std::cout << "MyContainer with custom allocator\n";
+    std::cout << "Вывод содержимого c1:\n";
+    std::cout << " ";
+    for (int x : c1)
+        std::cout << x << " ";
+    std::cout << "\n";
+
+    std::cout << "MyContainer с пользовательским аллокатором\n";
     MyContainer<int, MyAllocator<int, 10>> c2;
     for (int i = 0; i < 10; ++i)
         c2.push_back(i);
 
+    std::cout << "Вывод содержимого c2:\n";
+    std::cout << " ";
     for (int x : c2)
         std::cout << x << " ";
     std::cout << "\n";
